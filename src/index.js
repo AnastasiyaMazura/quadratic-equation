0baba3fb6836a92a9c0bd62ded9d973d0e94804c
AnastasiyaMazura <mazura.anastasiya@gmail.com>
2018-09-12 18:35:53 +0300

    all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let array = equation.split(' ');
+    let a = +array[0];
+    let b = +array[4];
+    if (array[3] === '-')
+    {
+        b *= (-1);
+    }
+    let c = +array[8];
+    if (array[7] === '-')
+    {
+        c *= (-1);
+    }
+
+    let D = b * b - 4 * a * c;
+    let x1 = Math.round((-b + Math.sqrt(D)) / (2 * a));
+    let x2 = Math.round((-b - Math.sqrt(D)) / (2 * a));
+    return [x1, x2].sort(function (a, b) {
+        return a > b ? 1 : -1;
+    });
 }
